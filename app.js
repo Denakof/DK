48c889602bf2586eced4397456b9950a1443aa01
denakof <>
2021-04-14 12:52:06 +0000

    addapp

---
+++ app.js
@@ -0,0 +1,30 @@
+alert ("for subscription press ok")
+
+var username = prompt ("enter your name please");
+alert ("welcome " + username)
+//prompt ("enter your name please")
+console.log("welcome " + username)
+
+
+var age = prompt ("enter your age please");
+
+
+var Gender = prompt ("enter your gender,(male or female)")
+
+
+
+
+var weight = prompt("insert your weight")
+if (weight > 110) {
+  console.log("you are obese,")
+  alert("you are obese, we are here to help you")
+} 
+
+else if (weight <=100) {"you are overweight"
+alert("you are overweight, we are here to help you")
+}
+
+
+alert ("we are waiting you at our place, happy to see you there")
+
+alert("are you excited!!")
