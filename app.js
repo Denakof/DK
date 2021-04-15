e2cab79d5d17d05b1b134bba49db0bbdebd1bba4
denakof <>
2021-04-15 19:13:29 +0000

    add

--- app.js
+++ app.js
@@ -2,29 +2,49 @@ alert ("for subscription press ok")
 
 var username = prompt ("enter your name please");
 alert ("welcome " + username)
-//prompt ("enter your name please")
+prompt ("enter your name please")
 console.log("welcome " + username)
 
 
 var age = prompt ("enter your age please");
 
 
-var Gender = prompt ("enter your gender,(male or female)")
+ var Gender = prompt ("enter your gender,(male or female)")
+ while(Gender !== "male" &&  Gender !== "female"){
+    Gender= prompt ("enter your gender,(male or female)")
+ }
+ if (Gender =="male"){
+   alert("Go to men's section")
+}
+ else if (Gender == "female"){
+   alert ("go to women's section")
+}
 
 
 
 
-var weight = prompt("insert your weight")
+
+var weight = Number (prompt("insert your weight"))
 if (weight > 110) {
   console.log("you are obese,")
   alert("you are obese, we are here to help you")
 } 
 
-else if (weight <=100) {"you are overweight"
-alert("you are overweight, we are here to help you")
-}
+
+else if(weight <=100) {"you are overweight"
+alert("you are overweight, we are here to help you")}
 
 
 alert ("we are waiting you at our place, happy to see you there")
 
+var duration = prompt ("will you subscribe for 1,2 or 3 months") 
+if (duration >=2)
+{ var image ='<img src= "https://www.hotelconcordiabarcelona.com/content/thumbs/1024/content/imgsxml/ofertas/amp-20-ciento-descuento-texto-3d-2227-126812.jpg" alt="image"/>';
+document.write(image)}
+
+else if (duration<2){ var image5 = '<img src="https://steamgamesbtc.com/pics/356/preview.png" alt="image5" />';document.write(image5);}
+
+
 alert("are you excited!!")
+
+confirm ("PRESS TO CONFIRM")
